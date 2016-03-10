ebfab163493273c26e8c85da7ccb292c0ca8ac49
Marc Sibson <sibson@gmail.com>
2016-03-10 11:21:03 -0800

    use emojis as source

--- t.py
+++ t.py
@@ -4,34 +4,38 @@ import sys
 import json
 
 
-def parse_dictionary(filelike):
-    data = json.loads(filelike.read())
-
+def parse_eac(filename='eac.json'):
+    data = json.load(open(filename))
     def _convert(code):
         try:
-            return int(code, 16)
+            return unichr(int(code, 16))
         except ValueError:
             return code
 
     return {v['alpha_code']: _convert(k) for k, v in data.iteritems()}
 
 
-def translate(codes):
-    with open('eac.json') as f:
-        lookup = parse_dictionary(f)
+def parse_emojis(filename='emojis.json'):
+    data = json.load(open(filename))
 
+    return {':{}:'.format(k): v['char'] for k, v in data.iteritems()}
+
+
+def translate(lookup, codes):
     output = []
     for em in sys.argv[1:]:
         if em[0] != ':' and em[-1] != ':':
             em = ':{}:'.format(em)
         code = lookup[em]
-        output.append(unichr(code))
+        output.append(code)
 
     return output
 
 
 def main():
-    output = translate(sys.argv[1:])
+    lookup = parse_emojis()
+
+    output = translate(lookup, sys.argv[1:])
     print(' '.join(output))
 
 
