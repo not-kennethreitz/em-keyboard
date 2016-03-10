7e6a0aae279a415931918295b5caaed986c04bee
Marc Sibson <sibson@gmail.com>
2016-03-10 11:53:02 -0800

    add basic alias support

--- t.py
+++ t.py
@@ -12,30 +12,40 @@ def parse_eac(filename='eac.json'):
         except ValueError:
             return code
 
-    return {v['alpha_code']: _convert(k) for k, v in data.iteritems()}
+    return {v['alpha_code'][1:-1]: _convert(k) for k, v in data.iteritems()}
 
 
 def parse_emojis(filename='emojis.json'):
     data = json.load(open(filename))
 
-    return {':{}:'.format(k): v['char'] for k, v in data.iteritems()}
+    return {k: v['char'] for k, v in data.iteritems()}
 
 
-def translate(lookup, codes):
+def parse_aliases(lookup, filename='aliases.json'):
+    data = json.load(open(filename))
+
+    return {k: [lookup[c] for c in v.split()] for k, v in data.iteritems()}
+
+
+def translate(lookup, aliases, codes):
     output = []
-    for em in sys.argv[1:]:
-        if em[0] != ':' and em[-1] != ':':
-            em = ':{}:'.format(em)
-        code = lookup[em]
-        output.append(code)
+    for code in sys.argv[1:]:
+        if code[0] == ':' and code[-1] == ':':
+            code = code[1:-1]
+
+        if code in aliases:
+            output.extend(aliases[code])
+        else:
+            output.append(lookup[code])
 
     return output
 
 
 def main():
     lookup = parse_emojis()
+    aliases = parse_aliases(lookup)
 
-    output = translate(lookup, sys.argv[1:])
+    output = translate(lookup, aliases, sys.argv[1:])
     print(' '.join(output))
 
 
