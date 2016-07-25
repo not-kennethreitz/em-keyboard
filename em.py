85caa3534c3d7caec45a5f8b38947249daf3a3d4
Kenneth Reitz <me@kennethreitz.org>
2016-07-25 01:04:44 -0400

    remove horrible alias feature

--- em.py
+++ em.py
@@ -4,7 +4,6 @@
 
 Usage:
   em <name>... [--no-copy]
-  em add <name> <character>...
 
 Options:
   -h --help     Show this screen.
@@ -18,10 +17,7 @@ Examples:
 Notes:
   - If all names provided map to emojis, the resulting emojis will be
     automatically added to your clipboard.
-  - Custom aliases for emoji sets are supported.
 """
-
-from __future__ import absolute_import, print_function, unicode_literals
 
 from collections import defaultdict
 import json
@@ -37,40 +33,28 @@ def parse_emojis(filename='emojis.json'):
     return json.load(open(filename))
 
 
-def parse_aliases(lookup, filename='aliases.json'):
-    data = json.load(open(filename))
-
-    return {k: [lookup[c] for c in v.split()] for k, v in data.iteritems()}
-
-
-def translate(lookup, aliases, code):
+def translate(lookup, code):
     output = []
     if code[0] == ':' and code[-1] == ':':
         code = code[1:-1]
 
-    if code in aliases:
-        output.extend(aliases[code])
-    else:
-        output.append(lookup.get(code, {'char': None})['char'])
+    output.append(lookup.get(code, {'char': None})['char'])
 
     return output
 
 
-def do_list(lookup, aliases, term):
-    """
-        matches term glob against short-name,
-    """
-    space = lookup.keys() + aliases.keys()
+def do_list(lookup, term):
+    """Matches term glob against short-name."""
 
+    space = lookup.keys()
     matches = fnmatch.filter(space, term)
 
-    return [(m, translate(lookup, aliases, m)) for m in matches]
+    return [(m, translate(lookup, m)) for m in matches]
 
 
-def do_find(lookup, aliases, term):
-    """
-        matches term glob against short-name, keywords and categories
-    """
+def do_find(lookup, term):
+    """Matches term glob against short-name, keywords and categories."""
+
     space = defaultdict(list)
 
     for name in lookup.keys():
@@ -81,16 +65,13 @@ def do_find(lookup, aliases, term):
             space[keyword].append(name)
         space[definition['category']].append(name)
 
-    for name in aliases.keys():
-        space[name].append(name)
-
     matches = fnmatch.filter(space.keys(), term)
 
     results = set()
     for match in matches:
         results.update(space[match])
 
-    return [(r, translate(lookup, aliases, r)) for r in results]
+    return [(r, translate(lookup, r)) for r in results]
 
 
 def cli():
@@ -99,30 +80,33 @@ def cli():
     names = arguments['<name>']
     no_copy = arguments['--no-copy']
 
-    if arguments['add']:
-        arguments['<charecter>']
-        arguments['<name>']
-
-
+    # Marker for if the given emoji isn't found.
     missing = False
 
+    # Grab the lookup dictionary.
     lookup = parse_emojis()
-    aliases = parse_aliases(lookup)
 
-    results = (translate(lookup, aliases, name) for name in names)
+    # Process the results.
+    results = (translate(lookup, name) for name in names)
     results = list(itertools.chain.from_iterable(results))
+
     if None in results:
         no_copy = True
         missing = True
         results = (r for r in results if r)
 
+    # Prepare the result strings.
     print_results = ' '.join(results)
     results = ''.join(results)
 
-    print(print_results)
-
+    # Copy the results (and say so!) to the clipboard.
     if not no_copy and not missing:
         xerox.copy(results)
+        print u'Copied! {}'.format(print_results)
+
+    # Script-kiddies.
+    else:
+        print(print_results)
 
     sys.exit(int(missing))
 
