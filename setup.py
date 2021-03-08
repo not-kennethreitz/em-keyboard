88e01776ca64899b51cf4f69de7e4ce2e5531b32
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-03-08 12:57:19 +0200

    Skip xerox requirement on Linux, installation isn't trivial

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     zip_safe=False,
     use_scm_version={"local_scheme": local_scheme},
     setup_requires=["setuptools_scm"],
-    install_requires=["docopt", "xerox; platform_system != 'Windows'"],
+    install_requires=["docopt", "xerox; platform_system == 'Darwin'"],
     extras_require={"tests": ["pytest", "pytest-cov"]},
     python_requires=">=3.6",
     project_urls={
