e25a18abdc03d348599dc5977f780dd48b2a57be
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-08-26 15:58:54 +0300

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-from codecs import open
-
 from setuptools import setup
 
 with open("README.md", encoding="utf-8") as f:
@@ -52,6 +50,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
